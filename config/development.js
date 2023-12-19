92fd1395fa1e2d7836c7519a352c6c0a46dd8664
Wout Van Cleemput <wout.vancleemput@student.hogent.be>
2023-12-19 21:27:30 +0100

    Database werkt nu op vichogent

--- config/development.js
+++ config/development.js
@@ -9,12 +9,21 @@ module.exports = {
 		maxAge: 3 * 60 * 60,
 	},
 
-    database: {
+	 database: {
+		client: 'mysql2',
+		host: 'vichogent.be',
+		port: 40043,
+		name: '181098wv',
+		username: '181098wv',
+		password: 'NM36BKtbGTtzQJMTwO6l',
+	},
+
+    /*database: {
 		client: 'mysql2',
 		host: 'localhost',
 		port: 3306,
 		name: 'frontweb',
 		username: 'root',
 		password: 'root123',
-	},
+	},*/
 };
